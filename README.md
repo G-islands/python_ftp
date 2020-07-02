78e910a4fe67f937a225906d60a4b7fe5318f661
G-islands <guyifan.chinese@outlook.com>
2020-07-02 22:02:52 +0800

    final ver.

--- README.md
+++ README.md
@@ -5,7 +5,3 @@ FTP Client in python. Simple implement of FTP.
 Only support basic file upload and download.It can be used to manage the server file.(The most basic function)
 
 This project is the homework in Computer Network course.
-
-TODO:
-
-- [ ] GUI
--- client.py
+++ client.py
@@ -1,21 +1,21 @@
 import socket
 import os
 import getpass
-import random
 import re
 
 
 class FTPClient:
     def __init__(self, addr='0.0.0.0'):
-        self.addr = addr
-        self.port = 21
-        self.Connected = False
+        self.addr = addr  # 连接服务器的IP地址
+        self.port = 21  # 连接服务器的命令端口
+        self.Connected = False  # 服务器连接状态
         self.debug = False
-        self.user = False
-        self.slient = False
+        self.user = False  # 服务器用户登陆状态
+        self.silent = False  # 客户端的各种状态
         if(addr != '0.0.0.0'):
-            self.open(addr)
+            self.open(addr)  # 可直接初始化时建立连接
 
+# 命令传送函数
     def send(self, message):
         try:
             if(self.debug):
@@ -25,10 +25,11 @@ class FTPClient:
         except:
             return
 
+# 命令接收函数
     def recv(self):
         try:
             data = self.sock.recv(1024).decode('ascii').replace('\r\n', '')
-            if(not self.slient):
+            if(not self.silent):
                 print(data)
             if(data[0] == '3' or data[0] == '4' or data[0] == '5'):
                 raise Exception('connection error')
@@ -36,39 +37,43 @@ class FTPClient:
         except:
             pass
 
+# 用户登陆函数，530为登陆失败的返回码
     def login(self):
         try:
             user = input('User ('+self.addr+':)')
             self.send('USER '+user)
             self.recv()
-            passwd = getpass.getpass('Password:')
+            passwd = getpass.getpass('Password:')  # 输入隐藏比密码
             self.send('PASS '+passwd)
             if(self.recv()[0:3] != '530'):
                 self.user = True
         except:
             print('login failed')
 
+# 连接远程服务器
     def open(self, addr):
         try:
-            socket.inet_aton(addr)
+            socket.inet_aton(addr)  # 检测IP合法性
         except:
             print('invalid ip')
             return
         try:
             if(self.Connected):
-                print('already connected to '+self.addr)
+                print('already connected to '+self.addr)  # 判断当前连接状态
                 return
             self.addr = addr
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-            self.sock.connect((self.addr, self.port))
+            self.sock.connect((self.addr, self.port))  # 建立连接
             self.Connected = True
             self.recv()
         except:
             print('Connect refused')
+            return
         self.send('OPTS UTF8 ON')
         self.recv()
-        self.login()
+        self.login()  # 默认连接建立时登陆
 
+# 断开连接函数
     def disconnect(self):
         try:
             self.send('quit')
@@ -80,6 +85,7 @@ class FTPClient:
         self.user = False
         self.sock.close()
 
+# FTP使用主动模式，发送PASV命令
     def PASV(self):
         try:
             self.send('PASV')
@@ -94,92 +100,100 @@ class FTPClient:
         except Exception as e:
             print(e)
 
+# 文件上传函数
     def put(self, filename):
         try:
-            if(os.path.exists(os.getcwd()+'/'+filename) == False):
-                print('file not exits')
+            if(os.path.exists(os.getcwd()+'/'+filename) is False):
+                print('file not exits')  # 检查本地文件是否存在
                 return
             dataAddr, dataport = self.PASV()
             datasock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-            datasock.connect((dataAddr, dataport))
+            datasock.connect((dataAddr, dataport))  # 建立数据连接
             self.send('STOR '+filename)
             self.recv()
             f = open(os.getcwd()+'/'+filename, 'rb')
-            while True:
+            while True:  # 文件分段发送
                 data = f.read(1024)
                 if(not data):
                     break
                 datasock.send(data)
-            datasock.close()
+            datasock.close()  # 关闭数据连接
             self.recv()
         except:
             print('Transfer failed')
 
+# 检查远程服务器文件是否存在
     def checkfileexit(self, filename):
         try:
-            self.slient = True
-            files = self.LS().split('\r\n')
+            self.silent = True
+            files = self.LS().split('\r\n')  # 通过LS命令读取远程目录的文件项
             flag = False
             for item in files:
                 if(item.find(filename) != -1 and item[0] != 'd'):
+                    # 判断是否有同名文件
                     flag = True
                     break
-            self.slient = False
+            self.silent = False
             if(not flag):
                 print('file not exits')
             return flag
         except:
             print('unknown error')
 
+# 检查远程服务器文件夹是否存在
     def checkdirexit(self, filename):
         try:
-            self.slient = True
-            files = self.LS().split('\r\n')
+            self.silent = True
+            files = self.LS().split('\r\n')  # 通过LS命令读取远程目录的文件项
             flag = False
             for item in files:
                 if(item.find(filename) != -1 and item[0] == 'd'):
+                    # 判断是否有同名文件夹
                     flag = True
                     break
-            self.slient = False
+            self.silent = False
             if(not flag):
                 print('directory not exits')
             return flag
         except:
             print('unknown error')
 
+# 下载函数，获得远程服务器的文件
     def get(self, filename):
+        # 判断文件是否存在
         if(not self.checkfileexit(filename)):
             return
         try:
             dataAddr, dataport = self.PASV()
             datasock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-            datasock.connect((dataAddr, dataport))
+            datasock.connect((dataAddr, dataport))  # 建立数据连接
             self.send('RETR '+filename)
             filesize = int(re.findall(
                 r'[(](.*)[)]', self.recv())[0].split(' ')[0])
             buffersize = 1024
             f = open(filename, 'wb')
-            while(filesize > 0):
+            while(filesize > 0):  # 分段接收数据
                 buffer = datasock.recv(buffersize)
                 f.write(buffer)
                 filesize = filesize-buffersize
             f.close()
-            datasock.close()
+            datasock.close()  # 关闭数据连接
             self.recv()
         except:
             print('Transfer failed')
 
+# 获取远程服务器的文件列表 文件列表通过数据通道接收
     def LS(self):
         try:
             dataAddr, dataport = self.PASV()
             datasock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-            datasock.connect((dataAddr, dataport))
+            datasock.connect((dataAddr, dataport))  # 数据连接建立
             self.send('LIST')
             self.recv()
             buffersize = 1024
-            buffer = datasock.recv(buffersize).decode('ascii')
+            buffer = datasock.recv(buffersize).decode('ascii')  # 接收数据
             datasock.close()
-            if(not self.slient):
+            if(not self.silent):
                 print(buffer)
             self.recv()
             return buffer
@@ -188,11 +202,11 @@ class FTPClient:
 
     def cmd(self):
         cmd = input('ftp>')
-        if(cmd == 'help'):
+        if(cmd == 'help'):  # 帮助命令
             print(
                 '''help     get help
 open     open a remote connection args:<IP>
-dcon     disconnect the remote connection
+discon     disconnect the remote connection
 login    login remote system
 status   client status
 quit     close connection
@@ -200,50 +214,50 @@ pwd      get remote working directory
 lcd      get local working directory
 put      upload file
 get      download file
-mkdir    create directory 
+mkdir    create directory
 rmdir    delete directory
 delete   delete file
 ls       list files'''
             )
-        elif(cmd == 'quit'):
+        elif(cmd == 'quit'):  # 关闭FTP客户端
             if(self.Connected):
                 self.disconnect()
             quit()
-        elif(cmd[0:4] == 'open'):
+        elif(cmd[0:4] == 'open'):  # 建立远程连接
             self.open(cmd.split(' ')[1])
-        elif(cmd == 'discon'):
+        elif(cmd == 'discon'):  # 断开远程连接
             self.disconnect()
-        elif(cmd == 'login'):
+        elif(cmd == 'login'):  # 登陆远程服务器
             self.login()
-        elif(cmd == 'status'):
+        elif(cmd == 'status'):  # 查看客户端状态
             print('connected: '+str(self.Connected), end=' ')
             print('login: '+str(self.user))
         else:
             if(not self.user):
                 print('please login first')
             else:
-                if(cmd == 'lcd'):
+                if(cmd == 'lcd'):  # 获取当前的本地路径
                     print(os.getcwd())
-                elif(cmd[0:3] == 'put'):
+                elif(cmd[0:3] == 'put'):  # 发送文件
                     self.put(cmd.split(' ')[1])
-                elif(cmd[0:3] == 'get'):
+                elif(cmd[0:3] == 'get'):  # 下载文件
                     self.get(cmd.split(' ')[1])
-                elif(cmd == 'pwd'):
+                elif(cmd == 'pwd'):  # 获取服务器的路径
                     self.send('XPWD')
                     self.recv()
-                elif(cmd[0:5] == 'mkdir'):
+                elif(cmd[0:5] == 'mkdir'):  # 创建文件夹
                     self.send('XMKD '+cmd.split(' ')[1])
                     self.recv()
-                elif(cmd[0:2] == 'cd'):
+                elif(cmd[0:2] == 'cd'):  # 打开文件夹
                     self.send('CWD '+cmd.split(' ')[1])
                     self.recv()
-                elif(cmd == 'ls'):
+                elif(cmd == 'ls'):  # 获取文件列表
                     self.LS()
-                elif(cmd[0:6] == 'delete'):
+                elif(cmd[0:6] == 'delete'):  # 删除文件
                     if(self.checkfileexit(cmd.split(' ')[1])):
                         self.send('DELE '+cmd.split(' ')[1])
                         self.recv()
-                elif(cmd[0:5] == 'rmdir'):
+                elif(cmd[0:5] == 'rmdir'):  # 删除文件夹
                     if(self.checkdirexit(cmd.split(' ')[1])):
                         self.send('RMD '+cmd.split(' ')[1])
                         self.recv()
